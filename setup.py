ba7436d1c9a95bc23aaaa64276aaa2b69c14078f
Peter Waller <peter@scraperwiki.com>
2013-10-10 08:58:59 +0100

    Initial commit

---
+++ setup.py
@@ -0,0 +1,24 @@
+from distutils.core import setup
+
+
+conf = dict(
+    name='inf',
+    version='1.0.0',
+    description="Infinity",
+    long_description="\n".join(list(open("README"))[2:]).strip(),
+    classifiers=[
+        "Development Status :: 6 - Mature",
+        "Intended Audience :: Developers",
+        "License :: OSI Approved :: MIT License",
+        "Programming Language :: Python",
+        "Topic :: Scientific/Engineering :: Mathematics",
+    ],
+    keywords='inf infinity',
+    author='Peter Waller',
+    author_email='p@pwaller.net',
+    url='http://github.com/pwaller/inf',
+    py_modules=["inf"],
+)
+
+if __name__ == '__main__':
+    setup(**conf)
